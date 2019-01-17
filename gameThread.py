b2e21986b942bb23658a687193304210d6b9bc35
zhangyu <yu.zhang@singou.mo>
2019-01-17 19:26:13 +0800

    update : 添加多线程

---
+++ gameThread.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import threading
+import time
+
+from uitls.game_utils import KungFuWorldUltimateChallenge
+
+
+class Game(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.tips = KungFuWorldUltimateChallenge()
+
+    def run(self):
+        print("开始线程：" + self.name)
+        print()
+        self.start_game()
+        print("退出线程：" + self.name)
+
+    def game(self):
+        self.tips.restart_fighting()
+        # 等待动画播放完成
+        time.sleep(10)
+        # 点击开始战斗
+        self.tips.start_fighting_one()
+        # 等待动画完成
+        time.sleep(8)
+        # 放置植物
+        self.tips.planting_peas()
+        # tips.planting_sunflowers_one()
+        self.tips.planting_sunflowers_two()
+        # tips.planting_sunflowers_three()
+        # tips.planting_sunflowers_four()
+
+        # 开始战斗
+        self.tips.start_fighting_two()
+        # 等待动画完成
+        time.sleep(3)
+        #  收集能量豆
+        self.tips.click_on_gold_coins()
+
+        # 使用能量豆
+        self.tips.using_peas_two()
+        self.tips.using_peas_two()
+        self.tips.using_peas_two()
+        self.tips.using_peas_two()
+        # tips.using_peas_four()
+        # tips.using_peas_two()
+        # tips.using_peas_three()
+
+        time.sleep(1)
+
+    def start_game(self):
+        for x in range(10000):
+            start = time.time()
+            self.game()
+            end = time.time()
+            print("total :", x, (end - start))
--- main.py
+++ main.py
@@ -1,54 +1,12 @@
 # -*- coding: utf-8 -*-
 
 #  程序主函数
-
-import time
-
-from uitls.game_utils import KungFuWorldUltimateChallenge
-
-
-def game():
-    tips.restart_fighting()
-    # 等待动画播放完成
-    time.sleep(10)
-    # 点击开始战斗
-    tips.start_fighting_one()
-    # 等待动画完成
-    time.sleep(8)
-    # 放置植物
-    tips.planting_peas()
-    # tips.planting_sunflowers_one()
-    tips.planting_sunflowers_two()
-    # tips.planting_sunflowers_three()
-    # tips.planting_sunflowers_four()
-
-    # 开始战斗
-    tips.start_fighting_two()
-    # 等待动画完成
-    time.sleep(3)
-    #  收集能量豆
-    tips.click_on_gold_coins()
-
-    # 使用能量豆
-    tips.using_peas_two()
-    tips.using_peas_two()
-    tips.using_peas_two()
-    tips.using_peas_two()
-    # tips.using_peas_four()
-    # tips.using_peas_two()
-    # tips.using_peas_three()
-
-    time.sleep(1)
-
-
-def main():
-    for x in range(1000):
-        start = time.time()
-        game()
-        end = time.time()
-        print("total :", x, (end - start))
-
+from gameThread import Game
 
 if __name__ == '__main__':
-    tips = KungFuWorldUltimateChallenge()
-    main()
+    print("main start")
+    game = Game("zhangyu")
+    game.start()
+    print()
+    print("main end")
+
